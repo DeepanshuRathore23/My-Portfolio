925ee8bed2e3c642c6ff2ddc4c9789dc6653d2d5
Deepanshu Rathore <deepanshurathore2004@gmail.com>
2024-01-22 16:39:25 +0530

    resolved issue

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+//time
 
-circleMouseFollower();
+var currentTime = document.querySelector("#current-time");
+function updateTime(){
+    let a = new Date();
+
+    let year = a.getFullYear();
+    let hour = a.getHours();
+    let minute = a.getMinutes();
+    let second = a.getSeconds();
+
+    currentTime.innerHTML = `${year}&copy    ${hour}:${minute}:${second}`;
+}
+
+setInterval(updateTime,1000);
+updateTime();
